701429228fee33f536d174a565df6fc06f6160af
Vijay Pillai <evijayanathan@gmail.com>
2026-01-19 19:58:26 -0500

    fix: create new migration to retry applying schema changes

--- prisma/migrations/20260119200000_add_missing_schema_changes/migration.sql
+++ prisma/migrations/20260119200000_add_missing_schema_changes/migration.sql
@@ -1,15 +1,1 @@
--- CreateEnum
-CREATE TYPE "FundraisingStatus" AS ENUM ('ACTIVE', 'CLOSED');
-
--- AlterEnum
-ALTER TYPE "TransactionType" ADD VALUE 'IBA_DEPOSIT';
-
--- AlterTable
-ALTER TABLE "FundraisingCampaign" ADD COLUMN "status" "FundraisingStatus" NOT NULL DEFAULT 'ACTIVE';
-
--- AlterTable
-ALTER TABLE "TroopSettings" ADD COLUMN "address" TEXT;
-
--- AlterTable
-ALTER TABLE "User" ADD COLUMN "preferredColor" TEXT DEFAULT 'orange',
-ADD COLUMN "preferredTheme" TEXT DEFAULT 'system';
+-- Reverted logic moved to 20260119200500 due to premature application of empty file
